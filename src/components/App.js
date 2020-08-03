a87611464419426c0f0a2ca7e3f94db83d91656a
aknair8 <akashkiran888@gmail.com>
2020-08-03 12:25:30 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    render() {
+
+        return (
+            <div>
+               React App
+            </div>
+        );
+    }
+}
+
+
+export default App; 
